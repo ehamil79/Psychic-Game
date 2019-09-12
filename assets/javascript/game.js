c052845fdecdea40c715f4e521bcf78c01dff563
Eric Hamil <ehamil79@gmail.com>
2019-09-12 12:45:56 -0400

    added some notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,6 @@ var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
 var wins = 0;
 var losses = 0;
 var left = 10;
-// var guesses = 10;
 var guessesSoFar = [];
 var psychicLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
@@ -33,9 +32,10 @@ document.onkeyup = function(event) {
 	var userGuess = event.key.toLowerCase();
     left--;
     if (guessesSoFar.includes(userGuess)) {
-     alert("Hey you already guessed" + userGuess);       
+     alert("Hey you already guessed " + userGuess);       
     }
-
+//  I want to change the alert to where when the user guesses the same letter twice nothing happens. Right now after the alert 
+// the letter gets added to the array twice
     guessesSoFar.push(userGuess);
     soFar();
     guessesLeft();
